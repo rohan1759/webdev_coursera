3a317bdfe3ead37c320af30c7f342749f4a73f40
rohan1759 <111270256+rohan1759@users.noreply.github.com>
2022-08-16 01:29:33 +0530

    Update script.js

--- mod4_sol/script.js
+++ mod4_sol/script.js
@@ -1,18 +1,13 @@
 
-(function () {
+(function (window) {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-  if (firstLetter === 'j') {
-    byeSpeaker(names[i]);
+  for (var i in names) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
   }
-  else {
-    helloSpeaker(names[i]);
-  }
-}
-
-})();
-
+})(window);
