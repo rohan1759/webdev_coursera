e29ee595a7db0c11a61f416a06396900cf3c28ea
rohan1759 <111270256+rohan1759@users.noreply.github.com>
2022-08-16 01:31:38 +0530

    Update SpeakHello.js

--- mod4_sol/SpeakHello.js
+++ mod4_sol/SpeakHello.js
@@ -1,9 +1,9 @@
-(function(window) {
-	var speakWord = "Hello";
-	var helloSpeaker = function (name) {
-		console.log(speakWord + " " + name);
-	}
-
-	window.helloSpeaker = helloSpeaker;
-
+(function (window) {
+  var speakWord = "Hello";
+  var helloSpeaker = {
+    speak: function (name) {
+      console.log(speakWord + " " + name);
+    }
+  };
+  window.helloSpeaker = helloSpeaker;
 })(window);
