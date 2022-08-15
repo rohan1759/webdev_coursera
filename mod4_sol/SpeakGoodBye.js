cc5a6c3e5ab0ea11c6df80b9955e34511d7bd7dc
rohan1759 <111270256+rohan1759@users.noreply.github.com>
2022-08-16 01:30:04 +0530

    Update SpeakGoodBye.js

--- mod4_sol/SpeakGoodBye.js
+++ mod4_sol/SpeakGoodBye.js
@@ -1,10 +1,9 @@
-
-(function(window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
-
+(function (window) {
+  var speakWord = "Good Bye";
+  var byeSpeaker = {
+    speak: function (name) {
+      console.log(speakWord + " " + name);
+    }
+  };
+  window.byeSpeaker = byeSpeaker;
 })(window);
